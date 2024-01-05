3b52ac6b9550cb8c3440874438633c39d12de315
Raimondo Marino <darkfenneer69@gmail.com>
2024-01-05 12:05:06 +0100

    itera more than 100 contacs

--- active_campaign.py
+++ active_campaign.py
@@ -10,202 +10,171 @@ from sib_api_v3_sdk.rest import ApiException
 load_dotenv()
 
 
-# active_url = os.getenv("active_url")
-# headers = {"accept": "application/json", "Api-Token": os.getenv("active_api")}
+def _get_leads_from_active():
+    print("\nGETTING DATA FROM ITERA DIGITAL...")
+    headers = {"accept": "application/json", "Api-Token": os.getenv("active_api")}
 
-# ## build dict with custom fields
-# url = str(active_url) + "fields"
-# response = requests.get(url, headers=headers, timeout=30)
-# resp = json.loads(response.text)
-# # pprint(resp["fields"], indent=2)
-
-# CUSTOM_FIELDS = {}
-# for field in resp["fields"]:
-#     # print(f"{field['id']}: {field['perstag']}")
-#     CUSTOM_FIELDS[field["id"]] = field["perstag"]
-#     # print(type(field["id"]))
-#     # print(type(field["perstag"]))
-
-# pprint(CUSTOM_FIELDS, indent=2)
-
-# with open("./data/itera_custom_fields.json", "w") as file:
-#     json.dump(CUSTOM_FIELDS, file)
-
-# url = "https://virtualvip.api-us1.com/api/3/contacts?listid=1"
-
-
-# response = requests.get(url, headers=headers, timeout=30)
-# resp = json.loads(response.text)
-
-# print("\nresp[contacts]:")
-# # pprint(resp["contacts"])
-
-# list_of_contacts = []
-# list_of_ids = []
-# for contact in resp["contacts"]:
-#     print("\ncontact:")
-#     print(contact)
-#     print("*" * 60)
-#     ids_dict = {}
-#     contact_dict = {}
-
-#     ids_dict[contact["email"]] = contact["id"]
-
-#     contact_dict["Name"] = contact["firstName"]
-#     contact_dict["Surname"] = contact["lastName"]
-#     contact_dict["Email"] = contact["email"]
-#     contact_dict["Telefono"] = contact["phone"]
-
-#     url = f"https://virtualvip.api-us1.com/api/3/contacts/{contact['id']}/fieldValues"
-#     attributes = requests.get(url, headers=headers, timeout=30)
-#     resp = json.loads(attributes.text)
-#     pprint(json.loads(attributes.text), indent=2)
-
-#     _dict = {}
-#     for contact in resp["fieldValues"]:
-#         print("\nprinting contact")
-#         pprint(contact, indent=2)
-#         # print(f"{CUSTOM_FIELDS[contact['field']]}: {contact['value']}")
-#         if contact["field"]:
-#             contact_dict[CUSTOM_FIELDS[contact["field"]]] = contact["value"]
-#         if contact["field"] in ["8", "9"]:
-#             _dict[contact["field"]] = contact["id"]
-
-#     ids_dict.update(_dict)
-#     list_of_ids.append(ids_dict)
-#     list_of_contacts.append(contact_dict)
-
-#     print("*" * 60)
-#     print("*" * 60)
-
-
-# data = pd.DataFrame.from_records(list_of_contacts)
-# print(data)
-# print(data.shape)
-# with open("./data/itera_contacts.json", "w") as file:
-#     json.dump(list_of_ids, file)
-################################################################################
-
-
-# UPDATE CONTACT ATTRIBUTES
-
-# # # load Customer "SALES_CHANNEL"
-# itera_customers = pd.read_csv("./data/itera_customers.csv", index_col=["Email"])
-# print(itera_customers)
-
-# # update only those Customers that have a NaN value in the "SALES_CHANNEL" column
-# to_update = itera_customers[itera_customers["SALES_CHANNEL"].isnull()]
-# print(to_update)
-
-# # load custom fields attributes
-# with open("./data/itera_custom_fields.json", "r") as file:
-#     customer_fields = json.load(file)
-
-# # load Customer id attributes
-# with open("./data/itera_contacts.json", "r") as file:
-#     customer_contacts = json.load(file)
-
-
-# CREATE A CUSTOM FIELD VALUE
-
-# url = "https://virtualvip.api-us1.com/api/3/fieldValues"
-
-# payload = {
-#     "fieldValue": {"contact": "37", "field": "8", "value": "test2"},
-#     "useDefaults": False,
-# }
-# headers = {
-#     "accept": "application/json",
-#     "content-type": "application/json",
-#     "Api-Token": os.getenv("active_api"),
-# }
-
-# response = requests.post(url, json=payload, headers=headers, timeout=30)
-
-# print(response.text)
-
-
-###### data from Leads with missing data #######################################
-
-headers = {"accept": "application/json", "Api-Token": os.getenv("active_api")}
-active_url = "https://virtualvip.api-us1.com/api/3/"
-
-## build dict with custom fields
-url = str(active_url) + "fields"
-try:
-    response = requests.get(url, headers=headers, timeout=30)
-    resp = json.loads(response.text)
-    # print()
-    # pprint(resp, indent=2)
-    # print()
-    custom_fields = {}
-    for field in resp["fields"]:
-        # print(f"{field['id']}: {field['perstag']}")
-        custom_fields[field["id"]] = field["perstag"]
-
-    # save custom fields for later contacts update
-    with open("./data/itera_custom_fields.json", "w") as file:
-        json.dump(custom_fields, file)
-
-    url = str(active_url) + "contacts?listid=2&limit=100"  # &id_greater=0&id_less=21"
+    ## build dict with custom fields
+    url = str(os.getenv("active_url")) + "fields"
     try:
         response = requests.get(url, headers=headers, timeout=30)
         resp = json.loads(response.text)
-        pprint(resp, indent=2)
-        print(f"\ntotal number of contacts: {resp['meta']['total']}")
-        print()
-        list_of_contacts = []
-        list_of_ids = []
+        custom_fields = {}
+        for field in resp["fields"]:
+            # print(f"{field['id']}: {field['perstag']}")
+            custom_fields[field["id"]] = field["perstag"]
 
-        for contact in resp["contacts"]:
-            ids_dict = {}
-            contact_dict = {}
-            url = str(active_url) + f"contacts/{contact['id']}/fieldValues"
-            attributes = requests.get(url, headers=headers, timeout=30)
+        # save custom fields for later contacts update
+        with open("./data/itera_custom_fields.json", "w") as file:
+            json.dump(custom_fields, file)
 
-            ids_dict[contact["email"]] = contact["id"]
+        # download contacts from list
+        # url = "https://virtualvip.api-us1.com/api/3/contacts?listid=1"
+        overall = pd.DataFrame()
+        ids_list = []
 
-            # pprint(json.loads(attributes.text), indent=2)
-            contact_dict["Nome"] = contact["firstName"]
-            contact_dict["Cognome"] = contact["lastName"]
-            contact_dict["Email"] = contact["email"]
-            contact_dict["Telefono"] = contact["phone"]
+        # priority of lists:
+        # list_id = 1: "VirtualVIP - all Leads"
+        # list_id = 4: "VirtualVIP - WhatsApp"
+        # list_id = 3: "VirtualVIP - Calendly Leads"
+        # list_id = 2: "VirtualVIP - all Leads Partial"
 
-            resp = json.loads(attributes.text)
-            _dict = {}
-            for contact in resp["fieldValues"]:
-                # print(custom_fields[contact["field"]])
-                if contact["field"]:
-                    contact_dict[custom_fields[contact["field"]]] = contact["value"]
-                if contact["field"] in ["8", "9"]:
-                    _dict[contact["field"]] = contact["id"]
+        # for list_id in [4, 1]:
+        for list_id in [1, 4, 3, 2]:
+            url = str(os.getenv("active_url")) + f"contacts?listid={list_id}&limit=100"
+            page = 0
+            overall_contacs = []
+            while True:
+                try:
+                    url_get = url + f"&offset={page}"
+                    response = requests.get(url_get, headers=headers, timeout=30)
+                    resp = json.loads(response.text)
+                    # print("*" * 80)
+                    # # print(resp)
+                    # print(len(resp["contacts"]))
+                    # print("*" * 80)
 
-            ids_dict.update(_dict)
-            list_of_ids.append(ids_dict)
+                    overall_contacs.extend(resp["contacts"])
 
-            list_of_contacts.append(contact_dict)
+                    if len(resp["contacts"]) == 0:
+                        break
+                    page += 100
+                except ApiException as error:
+                    print(
+                        f"ACTIVE: Exception when getting contacts from list : {error}\n"
+                    )
 
-        print(list_of_ids)
+            # print("*" * 80)
+            # print(overall_contacs)
+            # print(len(overall_contacs))
+            # print("*" * 80)
 
-        data = pd.DataFrame.from_records(list_of_contacts)
+            list_of_contacts = []
+            list_of_ids = []
 
-        # remove test users created with email: *@formulacoach.it before saving
-        data = data[~data["Email"].str.contains("formulacoach.it")]
+            for contact in overall_contacs:
+                ids_dict = {}
+                contact_dict = {}
+                url = (
+                    str(os.getenv("active_url"))
+                    + f"contacts/{contact['id']}/fieldValues"
+                )
 
-        print(data)
+                try:
+                    attributes = requests.get(url, headers=headers, timeout=30)
 
-        # to_save = data.set_index("Email")
-        # to_save = to_save[["SALES_CHANNEL"]]
+                    ids_dict[contact["email"]] = contact["id"]
+
+                    # pprint(json.loads(attributes.text), indent=2)
+                    contact_dict["Nome"] = contact["firstName"]
+                    contact_dict["Cognome"] = contact["lastName"]
+                    contact_dict["Email"] = contact["email"]
+                    contact_dict["Telefono"] = contact["phone"]
+
+                    resp = json.loads(attributes.text)
+                    _dict = {}
+                    for contact in resp["fieldValues"]:
+                        # print(custom_fields[contact["field"]])
+                        if contact["field"]:
+                            contact_dict[custom_fields[contact["field"]]] = contact[
+                                "value"
+                            ]
+                        if contact["field"] in ["8", "9"]:
+                            _dict[contact["field"]] = contact["id"]
+
+                    ids_dict.update(_dict)
+                    list_of_ids.append(ids_dict)
+
+                    list_of_contacts.append(contact_dict)
+
+                except ApiException as err:
+                    print(f"ACTIVE: Exception when getting attributes {err}\n")
+
+            # print("*" * 80)
+            # print(list_of_contacts)
+            # print(len(list_of_contacts))
+            # print("*" * 80)
+
+            # print(len(list_of_ids))
+            ids_list.append(list_of_ids)
+            data = pd.DataFrame.from_records(list_of_contacts)
+
+            # remove test users created with email: *@formulacoach.it before saving
+            data = data[~data["Email"].str.contains("formulacoach.it")]
+            data = data.set_index("Email")
+
+            if list_id == 1:
+                data["Form"] = "complete"
+            if list_id == 2:
+                data["Form"] = "missing"
+            elif list_id == 3:
+                data["Form"] = "calendly"
+            elif list_id == 4:
+                data["Form"] = "whatsup"
+            # print(data)
+            overall = pd.concat([overall, data])
+
+        # make it compatible with old code
+        _l = []
+        for sublist in ids_list:
+            _l.extend(sublist)
+
+        # print(list(_l))
+        # print(len(_l))
 
         # save IteraContacts as JSON file
-        # with open("./temp/itera_contacts_missing.csv", "w") as file:  # type: ignore
-        #     json.dump(list_of_ids, file)
+        with open(os.getenv("itera_contacts_missing"), "w") as file:  #    type: ignore
+            json.dump(_l, file)
 
+        # print(overall)
+        to_check = overall.sort_index()
+        if to_check.index.has_duplicates:
+            print("WARNING: duplicated records were deleted considering list priority")
+            # print(to_check[to_check.index.duplicated()])
+            df_reset = to_check.reset_index()
+            print("\nbefore removing:")
+            print(df_reset.head(15))
+            print(df_reset.shape)
+            df_reset.drop_duplicates(subset=["Email"], keep="first", inplace=True)
+            df_reset.set_index("Email", inplace=True)
+            # print("\nafter removing:")
+            # print(df_reset.head(15))
+            overall = df_reset
+
+            # contacts_to_del = list(to_check[to_check.index.duplicated()].index)
+            # overall.drop_duplicates(index=contacts_to_del, inplace=True, keep="last")
+
+        to_save = overall[["SALES_CHANNEL"]]
         # for later use when updating Customer Contacts
-        # to_save.to_csv("./temp/itera_customers_missing.csv")
+        to_save.to_csv(os.getenv("itera_customers_missing"))
+
+        return overall
 
     except ApiException as err:
-        print(f"ACTIVE: Exception when getting contacts from list : {err}\n")
+        print(f"ACTIVE: Exception when getting fields : {err}\n")
 
-except ApiException as err:
-    print(f"ACTIVE: Exception when calling custom fields: {err}\n")
+
+if __name__ == "__main__":
+    contacts = _get_leads_from_active()
+    print(contacts.head(15))
+    print(contacts.shape)
--- main copy.py
+++ main copy.py
@@ -99,22 +99,22 @@ class DataLeads:
         raw_data = self._get_leads_from_active()
         itera_data = self._active_data_pre_processing(raw_data)
 
-        #########   NETING data   #############
-        neting_data = self._get_leads_from_brevo()
+        # #########   NETING data   #############
+        # neting_data = self._get_leads_from_brevo()
 
-        overall = self._overall_customers(itera_data, neting_data)
-        #########   VirtualVIP data   ########
-        final_dataset = self._get_vvip_reporting(overall)
+        # overall = self._overall_customers(itera_data, neting_data)
+        # #########   VirtualVIP data   ########
+        # final_dataset = self._get_vvip_reporting(overall)
 
-        # # #########   UPDATE Campaigns   ########
-        self._update_neting(final_dataset)
-        self._update_itera(final_dataset)
+        # # # #########   UPDATE Campaigns   ########
+        # self._update_neting(final_dataset)
+        # self._update_itera(final_dataset)
 
-        ############# UPDATE PROMO   ##############
-        self._update_backoffice(final_dataset)
+        # ############# UPDATE PROMO   ##############
+        # self._update_backoffice(final_dataset)
 
-        # ########   Generate Reporting   ########
-        report = self._output_reports(final_dataset)
+        # # ########   Generate Reporting   ########
+        # report = self._output_reports(final_dataset)
 
     def _authenticate(self, client_id) -> str:
         print("\nVirtualVIP Backoffice data")
@@ -193,60 +193,29 @@ class DataLeads:
 
             # download contacts from list
             # url = "https://virtualvip.api-us1.com/api/3/contacts?listid=1"
+
             overall = pd.DataFrame()
             ids_list = []
-
-            # priority of lists:
-            # list_id = 1: "VirtualVIP - all Leads"
-            # list_id = 4: "VirtualVIP - WhatsApp"
-            # list_id = 3: "VirtualVIP - Calendly Leads"
-            # list_id = 2: "VirtualVIP - all Leads Partial"
-
-            # for list_id in [4, 1]:
-            for list_id in [1, 4, 3, 2]:
+            # for list_id in [1, 2, 3]:
+            for list_id in [2]:
                 url = (
                     str(os.getenv("active_url"))
                     + f"contacts?listid={list_id}&limit=100"
                 )
-                page = 0
-                overall_contacs = []
-                while True:
-                    try:
-                        url_get = url + f"&offset={page}"
-                        response = requests.get(url_get, headers=headers, timeout=30)
-                        resp = json.loads(response.text)
-                        # print("*" * 80)
-                        # # print(resp)
-                        # print(len(resp["contacts"]))
-                        # print("*" * 80)
+                try:
+                    response = requests.get(url, headers=headers, timeout=30)
+                    resp = json.loads(response.text)
 
-                        overall_contacs.extend(resp["contacts"])
+                    list_of_contacts = []
+                    list_of_ids = []
 
-                        if len(resp["contacts"]) == 0:
-                            break
-                        page += 100
-                    except ApiException as error:
-                        print(
-                            f"ACTIVE: Exception when getting contacts from list : {error}\n"
+                    for contact in resp["contacts"]:
+                        ids_dict = {}
+                        contact_dict = {}
+                        url = (
+                            str(os.getenv("active_url"))
+                            + f"contacts/{contact['id']}/fieldValues"
                         )
-
-                # print("*" * 80)
-                # print(overall_contacs)
-                # print(len(overall_contacs))
-                # print("*" * 80)
-
-                list_of_contacts = []
-                list_of_ids = []
-
-                for contact in overall_contacs:
-                    ids_dict = {}
-                    contact_dict = {}
-                    url = (
-                        str(os.getenv("active_url"))
-                        + f"contacts/{contact['id']}/fieldValues"
-                    )
-
-                    try:
                         attributes = requests.get(url, headers=headers, timeout=30)
 
                         ids_dict[contact["email"]] = contact["id"]
@@ -273,32 +242,28 @@ class DataLeads:
 
                         list_of_contacts.append(contact_dict)
 
-                    except ApiException as err:
-                        print(f"ACTIVE: Exception when getting attributes {err}\n")
+                    # print(len(list_of_ids))
+                    ids_list.append(list_of_ids)
+                    data = pd.DataFrame.from_records(list_of_contacts)
 
-                # print("*" * 80)
-                # print(list_of_contacts)
-                # print(len(list_of_contacts))
-                # print("*" * 80)
+                    # remove test users created with email: *@formulacoach.it before saving
+                    data = data[~data["Email"].str.contains("formulacoach.it")]
+                    data = data.set_index("Email")
+                    # if list_id == 1:
+                    #     data["Form"] = "complete"
+                    if list_id == 2:
+                        data["Form"] = "missing"
+                    # elif list_id == 3:
+                    #     data["Form"] = "calendly"
+                    # elif list_id == 4:
+                    #     data["Form"] = "whatsup"
+                    # print(data)
+                    overall = pd.concat([overall, data])
 
-                # print(len(list_of_ids))
-                ids_list.append(list_of_ids)
-                data = pd.DataFrame.from_records(list_of_contacts)
-
-                # remove test users created with email: *@formulacoach.it before saving
-                data = data[~data["Email"].str.contains("formulacoach.it")]
-                data = data.set_index("Email")
-
-                if list_id == 1:
-                    data["Form"] = "complete"
-                if list_id == 2:
-                    data["Form"] = "missing"
-                elif list_id == 3:
-                    data["Form"] = "calendly"
-                elif list_id == 4:
-                    data["Form"] = "whatsup"
-                # print(data)
-                overall = pd.concat([overall, data])
+                except ApiException as err:
+                    print(
+                        f"ACTIVE: Exception when getting contacts from list : {err}\n"
+                    )
 
             # make it compatible with old code
             _l = []
@@ -317,22 +282,8 @@ class DataLeads:
             # print(overall)
             to_check = overall.sort_index()
             if to_check.index.has_duplicates:
-                print(
-                    "WARNING: duplicated records were deleted considering list priority"
-                )
-                # print(to_check[to_check.index.duplicated()])
-                df_reset = to_check.reset_index()
-                # print("\nbefore removing:")
-                # print(df_reset.head(15))
-                # print(df_reset.shape)
-                df_reset.drop_duplicates(subset=["Email"], keep="first", inplace=True)
-                df_reset.set_index("Email", inplace=True)
-                # print("\nafter removing:")
-                # print(df_reset.head(15))
-                overall = df_reset
-
-                # contacts_to_del = list(to_check[to_check.index.duplicated()].index)
-                # overall.drop_duplicates(index=contacts_to_del, inplace=True, keep="last")
+                print("WARNING DUPLICATED RECORDS IN ITERA LISTS")
+                raise ValueError("ITERA data contain duplicated emails !!!")
 
             to_save = overall[["SALES_CHANNEL"]]
             # for later use when updating Customer Contacts
@@ -341,7 +292,7 @@ class DataLeads:
             return overall
 
         except ApiException as err:
-            print(f"ACTIVE: Exception when getting fields : {err}\n")
+            print(f"ACTIVE: Exception when calling custom fields: {err}\n")
 
     def _active_data_pre_processing(self, data: pd.DataFrame | None):
         if isinstance(data, type(None)):
@@ -1084,4 +1035,4 @@ if __name__ == "__main__":
     # Lead Stats
     # leads()
 
-    ### T.B.I.: (1) aggiunta di contatto fake ogni 500 per Brevo (per bug del sistema) (2): mandare in asincrono gli aggiornamenti su Itera
+    ### T.B.I.: (1) aggiunta di contatto fake ogni 500 per Brevo (per bug del sistema), (2): numero contatti maggiore di 100 per Active Campaign, (3): mandare in asincrono gli aggiornamenti su Itera
